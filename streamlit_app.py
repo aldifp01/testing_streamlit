1175787a5b394c6a43579159a0c672c6163f9e48
Aldi Fianda Putra <haisayaaldifp@gmail.com>
2023-08-04 11:00:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,16 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import altair as alt
 
-st.title('Uber pickups in NYC')
+st.title('Hasil Balapan Orang Paling Rasis di F1')
+racist=pd.read_csv("racistTr.csv",sep=",")
+alt.Chart(racist).transform_fold(
+    ['Charles Leclerc', 'Max Verstappen', 'Lewis Hamilton']
+).mark_bar().encode(
+    x='Laps',
+    y='Racist',
+    color='key:N'
+)
+
+st.markdown("""Charles Leclerc adalah yang paling rasis di Bahrain 2022.""")
